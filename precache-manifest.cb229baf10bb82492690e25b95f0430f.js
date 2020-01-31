b3e64a60d8d55aaa02135b1053a524bbf8f997cf
Artsiom_Reutovich <artsiom_reutovich@epam.com>
2020-01-31 18:02:29 +0300

    [no-ticket] build production

---
+++ precache-manifest.cb229baf10bb82492690e25b95f0430f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "846e48b0593c80c82541feb18959b8ea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a5a66dc024feeff8dbd3",
+    "url": "/static/css/main.ee2379c3.chunk.css"
+  },
+  {
+    "revision": "6316febd92c721c9e493",
+    "url": "/static/js/2.cc2e8c17.chunk.js"
+  },
+  {
+    "revision": "a5a66dc024feeff8dbd3",
+    "url": "/static/js/main.60759de4.chunk.js"
+  },
+  {
+    "revision": "75c03a92ed3f06fd02ad",
+    "url": "/static/js/runtime-main.84f25206.js"
+  }
+]);
